6bc5ff94db0210d1c05186869d6bf63553f4f058
Roger Taracha <rojtaracha@gmail.com>
2017-10-17 17:33:29 +0300

    [FEATURE] Interface Communication: [+] Defining an interface in the MasterListFragment class, and have it "callback" or communicate up to it's host activity.
    [+] In the MasterListFragment class, define a interface OnImageClickListener that triggers a callback in the host activity.
    [+] Override onAttach to make sure that the container activity has implemented the callback.
    [+] Set a click listener on the gridView and trigger the callback onImageSelected when an item is clicked.
    [+] In MainActivity, implement the MasterListFragment callback: OnImageClickListener
    [+] Define the behavior for the onImageSelected callback method and create a Toast that displays the position clicked.

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.android_me.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
@@ -9,11 +10,18 @@ import com.example.android.android_me.R;
  * Created by nabwera on 03/08/2017.
  */
 
-public class MainActivity extends AppCompatActivity {
+// This activity is responsible for displaying the master list of images
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Define the behaviour for onImageSelected
+    public void onImageSelected(int position) {
+        // Create a Toast that displays the position that was clicked
+        Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
     }
 }
